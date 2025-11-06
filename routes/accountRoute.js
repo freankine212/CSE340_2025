c95678a164a04c067254352a1cb9ee9990bd3944
freankine212 <lilmichaeljr@gmail.com>
2025-11-05 20:25:31 -0700

    starting week 8 assignment

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 
 //route to build the my account view
@@ -13,5 +14,22 @@ router.get("/register", utilities.handleErrors(accountController.buildAccount))
 
 //Even more registration route
 router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+// process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
+
 
 module.exports = router;
